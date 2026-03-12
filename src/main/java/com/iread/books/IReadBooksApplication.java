4486763c65270f61c2dfc903de5ff02c0c215c88
kurmam12 <kurmam1212@outlook.com>
2026-03-12 11:15:12 +0530

    Add files via upload
    IRead online books reading project

---
+++ src/main/java/com/iread/books/IReadBooksApplication.java
@@ -0,0 +1,13 @@
+package com.iread.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IReadBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IReadBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iread/books/IReadBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iread.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IReadBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
